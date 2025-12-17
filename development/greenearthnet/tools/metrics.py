a4b68591e0d16dc016ba9e755b3eea16dc55e013
dongzhang04 <d04zhang@gmail.com>
2025-12-16 21:32:16 -0800

    more work on the metrics code

--- development/greenearthnet/tools/metrics.py
+++ development/greenearthnet/tools/metrics.py
@@ -6,9 +6,30 @@ import seaborn as sns
 def ssim_wrapper(img1, img2):
     return ssim(img1, img2, data_range=2.0)
 
-def calculate_metrics(input_nc, reference_nc, roi_lat, roi_lon, subsampling_factor=1):
+def calculate_metrics(input_nc, reference_nc, roi_lat=None, roi_lon=None, subsampling_factor=1):
     input_ndvi = xr.open_dataset(input_nc)["ndvi_pred"]
-    reference_ndvi = xr.open_dataset(reference_nc)["ndvi"]
+    reference = xr.open_dataset(reference_nc)
+    reference_B4 = reference["s2_B04"]
+    dates = reference.time.values
+    
+    for i in range(5): 
+        if not reference_B4.sel(time=dates[i]).isnull().all().item():
+            offset = i
+            break
+    
+    for i in range(dates.size):
+        if i < offset:
+            reference = reference.drop_sel(time=dates[i])
+        else:
+            offset += 5
+            if offset <55:
+                reference = reference.drop_sel(time=dates[i])
+    
+    reference["ndvi"] = (("time", "x", "y"), np.zeros((20, 128, 128)))
+    reference["ndvi"] = (reference["s2_B8A"] - reference["s2_B04"]) / xr.where((reference["s2_B8A"] + reference["s2_B04"]) == 0, np.nan, (reference["s2_B8A"] + reference["s2_B04"]))
+    reference_ndvi = reference["ndvi"]
+
+    # input_ndvi = reference_ndvi
     residuals = input_ndvi - reference_ndvi
     U = np.sqrt((residuals**2).mean(dim=("lat", "lon"))) #RMSE
     A = (residuals).mean(dim=("lat", "lon"))
@@ -33,7 +54,10 @@ def calculate_metrics(input_nc, reference_nc, roi_lat, roi_lon, subsampling_fact
 def plot_metrics():
     pass
 
-
+if __name__ == "__main__":
+    input = r"E:\DZ\predictions\contextformer6M_seed42\MJJ21\minicube_0_29SND_39.29_-8.56.nc"
+    reference = r"E:\DZ\greenearthnet\ood-t_chopped\MJJ21\minicube_0_29SND_39.29_-8.56.nc"
+    print(calculate_metrics(input, reference))
 
 
 
